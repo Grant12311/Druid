bcbe345ca7c4933d656f11387861b261da9333cd
Grant Joseph <grantj150@yahoo.com>
2020-10-19 13:41:20 -0400

    add initial premake5.lua file

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+workspace("Druid")
+    language("C++")
+    toolset("gcc")
+    configurations({"Debug", "Release"})
+    platforms({"LinuxARM"})
+
+    links({"GL"})
+
+    buildoptions({"-std=c++1z", "-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
+
+    filter("platforms:LinuxARM")
+        architecture("ARM")
+        system("linux")
+
+    filter("configurations:Debug")
+        symbols("On")
+        buildoptions({"-pg"})
+        linkoptions({"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"})
+
+    filter("configurations:Release")
+        optimize("On")
+        flags({"LinkTimeOptimization"})
+
+    project("Druid")
+        filename("Druid")
+        kind("StaticLib")
+
+        files({"premake5.lua", ".gitignore", "src/*", "include/*"})
