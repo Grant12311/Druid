7d628643bce913ba13230ef0e1c4dc365d123a72
Grant Joseph <grantj150@yahoo.com>
2020-10-20 19:29:58 -0400

    seperate Druid project from workspace

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,8 @@
 workspace("Druid")
-    language("C++")
-    toolset("gcc")
     configurations({"Debug", "Release"})
     platforms({"LinuxARM"})
 
-    includedirs({"include"})
-    links({"GL"})
-
-    buildoptions({"-std=c++1z", "-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
+    buildoptions({"-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
 
     filter("platforms:LinuxARM")
         architecture("ARM")
@@ -23,9 +18,4 @@ workspace("Druid")
         flags({"LinkTimeOptimization"})
         defines({"DRUID_DISABLE_GLCALL"})
 
-    project("Druid")
-        filename("Druid")
-        kind("StaticLib")
-        targetdir("bin/%{cfg.platform}/%{cfg.buildcfg}")
-
-        files({"premake5.lua", "premake5-for-including.lua", ".gitignore", "src/**", "include/**"})
+    include("project.lua")
---
+++ project.lua
@@ -0,0 +1,12 @@
+project("Druid")
+    language("C++")
+    cppdialect("C++17")
+    toolset("gcc")
+    filename("Druid")
+    kind("StaticLib")
+    targetdir("bin/%{cfg.platform}/%{cfg.buildcfg}")
+
+    includedirs({"include"})
+    links({"GL"})
+
+    files({"premake5.lua", "premake5-for-including.lua", ".gitignore", "src/**", "include/**"})
