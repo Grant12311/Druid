d59862056fc7c1a1c6bf9184cfad76d8f8cd4f7f
Grant Joseph <granetejo@gmail.com>
2021-03-27 16:09:39 -0400

    Moderize Premake5 file

--- premake5.lua
+++ premake5.lua
@@ -1,21 +1,43 @@
-workspace("Druid")
-    toolset(gcc)
-    configurations({"Debug", "Release"})
-    platforms({"LinuxARM"})
+workspace "Druid"
+    language "C++"
+    cppdialect "C++17"
+    configurations {"Debug", "Release"}
+    platforms {"LinuxX64", "LinuxX86", "LinuxARM", "WindowsARM", "WindowsX86", "WindowsX64"}
+    startproject "Name"
 
-    buildoptions({"-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
+    flags {"MultiProcessorCompile"}
+    buildoptions {"-pipe", "-Wall"}
+    defines {}
 
-    filter("platforms:LinuxARM")
-        architecture("ARM")
-        system("linux")
+    links {}
+    includedirs {"%{wks.location}/include/"}
 
-    filter("configurations:Debug")
-        symbols("On")
-        buildoptions({"-pg"})
-        linkoptions({"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"})
+    filter "action:vs*"
+        toolset "msc"
 
-    filter("configurations:Release")
-        optimize("On")
-        defines({"DRUID_DISABLE_GLCALL"})
+    filter "action:not vs*"
+        toolset "gcc"
+        buildoptions {"-Wextra", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code", "-Wshadow", "-Wconversion", "-Wno-switch", "-Wno-unused-variable"}
 
-    include("project.lua")
+    filter "platforms:Linux*"
+        system "linux"
+    filter "platforms:Windows*"
+        system "windows"
+
+    filter "platforms:*ARM"
+        architecture "ARM"
+    filter "platforms:*X86"
+        architecture "x86"
+    filter "platforms:*X64"
+        architecture "x86_64"
+
+    filter "configurations:Debug"
+        symbols "On"
+        buildoptions {"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"}
+        linkoptions {"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"}
+
+    filter "configurations:Release"
+        optimize "On"
+        defines {"DRUID_DISABLE_GLCALL"}
+
+    include "project.lua"
--- project.lua
+++ project.lua
@@ -1,11 +1,11 @@
-project("Druid")
-    language("C++")
-    cppdialect("C++17")
-    filename("Druid")
-    kind("StaticLib")
-    targetdir("bin/%{cfg.platform}/%{cfg.buildcfg}")
+project "Druid"
+    language "C++"
+    cppdialect "C++17"
+    filename "Druid" 
+    kind "StaticLib"
+    targetdir "bin/%{cfg.platform}/%{cfg.buildcfg}"
 
-    includedirs({"include"})
-    links({"GL"})
+    includedirs {"include"}
+    links {"GL"}
 
-    files({"premake5.lua", "premake5-for-including.lua", ".gitignore", "src/**", "include/**"})
+    files {"premake5.lua", "premake5-for-including.lua", ".gitignore", "src/**", "include/**"}
